1ab61e22f2b39a4d1c5d23be8265d82ec080bad2
raichan <violetevargarde@gmail.com>
2025-08-30 09:06:32 +0700

    ayu

--- src/api.js
+++ src/api.js
@@ -1,2 +1,2 @@
-export const BASE_API = "http://192.168.94.41:3000";
-// export const BASE_API = "https://api-artikel-delta.vercel.app";
+// export const BASE_API = "http://192.168.94.41:3000";
+export const BASE_API = "https://api-artikel-delta.vercel.app";
